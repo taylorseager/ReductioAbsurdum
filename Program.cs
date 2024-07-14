ab01bf19595c7e3db4b70ce8aa5167671f2d10fc
Taylor Seager <taylorseager718@gmail.com>
2024-07-14 15:17:48 -0500

    greting and menu options added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string greeting = @"Welcome to Reductio & Absurdum Magic Shop!
+How can we help you today?";
+Console.WriteLine(greeting);
+
+string choice = null;
+while (choice != "0")
+{
+    Console.WriteLine(@"Choose an option:
+                        0. Exit
+                        1. View All Products
+                        2. Add Product
+                        3. Update a Product
+                        4. Delete Product");
+}
